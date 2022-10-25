4eae2e0acb62798abd0b8a13ac3deef21106d810
Osifo John <osifojohntec@gmail.com>
2022-10-25 23:25:50 +0100

    added job tracking app

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,6 +10,16 @@ export const projects = [
     id: 0,
   },
   {
+    title: "Job Tracking App",
+    description:
+      "Most routes in this app is protected, it's only when a user is logged that pages can be rendered and resoures from the server can be accessed. A user can update his profile and, create, read, update and delete job from the server. This app also has sort, filter funtionality for jobs and  the charts of pending applications, interviews scheduled, and jobs declined is displayed using react rechart library",
+    image: "./images/job-img.jpg",
+    tags: ["React", "Redux", "Styled-Components"],
+    source: "https://job-site-app-osifojohn.netlify.app",
+    visit: "https://github.com/osifojohn/job_tracking_app_redux-toolkit",
+    id: 1,
+  },
+  {
     title: "Movie App",
     description:
       "This is a movie web app with infinite scroll functionality so that users don’t need to wait for movies to preload which keep the user engaged with the page by presenting them with new as they scroll down.",
@@ -17,6 +27,6 @@ export const projects = [
     tags: ["React", "JavaScript", "CSS"],
     source: "https://reactjs-movieapp-osifojohn.netlify.app",
     visit: "https://github.com/osifojohn/reactjs-movieapp",
-    id: 1,
+    id: 2,
   },
 ];
