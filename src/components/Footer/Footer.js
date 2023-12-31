31eb0aa2e31dc701bb2644cd2c46e98a12a8a879
Osifo John <99277177+osifojohn@users.noreply.github.com>
2023-12-31 14:33:43 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -32,7 +32,7 @@ const Footer = () => {
         <LinkColumn>
           <LinkTitle>Resume</LinkTitle>
           <LinkItem
-            href="https://drive.google.com/file/d/1KyOllHe4jn2qSIYqmbKJ_qpEQj_sLsq9/view?usp=sharing"
+            href="https://drive.google.com/file/d/1d7ES3OBNlrWy11LMN-wBo4USZUXDBFQ4/view?usp=sharing"
             target="_blank"
           >
             drive.google.com/file/...
